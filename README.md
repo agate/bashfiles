0930b49f8e03e7f89bf1b3a155840765c47eff7c
Hao Hong <agate.hao@gmail.com>
2013-11-15 15:13:54 +0800

    added README.md

---
+++ README.md
@@ -0,0 +1,10 @@
+#My Bash Config Files
+
+## How to Install
+
+### Manually
+```
+$ git clone git://github.com/agate/bashfiles.git ~/.bash
+$ cd ~/.bash
+$ ./install.sh
+```
--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 BASH_DIR=$HOME/.bash
-DOT_DIR=$BASH_DIR/dot
+# DOT_DIR=$BASH_DIR/dot
 
 ln -f -i -s $BASH_DIR/bash_profile $HOME/.bash_profile
 ln -f -i -s $BASH_DIR/bashrc $HOME/.bashrc
 
-for file in `ls $DOT_DIR`; do
-  ln -f -i -s $DOT_DIR/$file $HOME/.$file
-done
+# for file in `ls $DOT_DIR`; do
+  # ln -f -i -s $DOT_DIR/$file $HOME/.$file
+# done
